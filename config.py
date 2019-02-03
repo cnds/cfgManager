a597f4118d08334ee946a46213b4afc4d41038db
d <ding.song@qianka.com>
2019-02-03 18:19:12 +0800

    add logger config

--- config.py
+++ config.py
@@ -10,9 +10,9 @@ class DevConfig(Config):
     SQLALCHEMY_BINDS = {
         'local': 'mysql://root@127.0.0.1:3307/zhuanqian?charset=utf8',
         'fp01': 'mysql://root@fp01.ops.gaoshou.me:3307/zhuanqian?charset=utf8',
-        'fp02': 'mysql://root@fp01.ops.gaoshou.me:3307/zhuanqian?charset=utf8',
-        'fp03': 'mysql://root@fp01.ops.gaoshou.me:3307/zhuanqian?charset=utf8',
-        'fp04': 'mysql://root@fp01.ops.gaoshou.me:3307/zhuanqian?charset=utf8'
+        'fp02': 'mysql://root@fp02.ops.gaoshou.me:3307/zhuanqian?charset=utf8',
+        'fp03': 'mysql://root@fp03.ops.gaoshou.me:3307/zhuanqian?charset=utf8',
+        'fp04': 'mysql://root@fp04.ops.gaoshou.me:3307/zhuanqian?charset=utf8'
     }
 
 
